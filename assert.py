1ca1a0d4eb5b4104ff99808c243b7a7aefd9e152
Lizbeth Videau <mothermary5951@gmail.com>
2015-04-01 12:14:14 -0400

    assert command example

---
+++ assert.py
@@ -0,0 +1,21 @@
+ 
+print "What is 4 + 4?",
+
+
+answer = raw_input()
+
+#  print type(answer)
+
+if int(answer) == 8:
+
+    print "You're right!"
+
+elif int(answer) != 8:
+
+    assert False, "WRONG!"
+
+
+
+
+
+
