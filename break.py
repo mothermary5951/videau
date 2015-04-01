094eb798bdfe555f7a4befd7ee023ca980eb1312
Lizbeth Videau <mothermary5951@gmail.com>
2015-04-01 14:57:08 -0400

    break to stop loops

---
+++ break.py
@@ -0,0 +1,14 @@
+for letter in 'alphabetize':    # first part of 2 part subprogram
+    if letter == 't':
+        break
+    print "current letter : ", letter
+
+num = 10                   # second part of little subprogram
+while num > 0:
+    print "current number : ", num
+    num = num -1
+    if num == 6:
+        break
+
+print "The end"
+
