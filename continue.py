d175f10b93716a60dac542bfdabca09fceca7fed
Lizbeth Videau <mothermary5951@gmail.com>
2015-04-01 16:40:50 -0400

    continue statement similar to break

---
+++ continue.py
@@ -0,0 +1,13 @@
+for letter in 'alphabetize':    # first part of 2 part subprogram
+    if letter == 'e':
+        continue
+    print "current letter : ", letter
+
+num = 12                   # second part of little subprogram
+while num > 0:x
+    num = num -1
+    if num == 8:
+        continue
+    print 'Current variable value :', num
+
+print "All done!"
