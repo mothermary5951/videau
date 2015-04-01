58ed5bc660a00e99d786b5c004e9dcefaa0187ce
Lizbeth Videau <mothermary5951@gmail.com>
2015-04-01 16:49:01 -0400

    classes example with string commneted out

---
+++ repaired_ classes_example.py
@@ -0,0 +1,23 @@
+class Grad:
+    "Common records for all grad students"
+    studentCount = 0
+
+    def __init__(self, name, school):
+        self.name = name
+        self.school = school
+        Grad.studentCount += 1
+
+    def displayCount(self):
+        print "Total Grad %d" % Grad.studentCount
+
+    def displayGrad(self):
+        print "Name : ", self.name, ", School: ", self.school
+
+# "This would create first object of Grad class"
+student1 = Grad("Christopher", "Case Western")
+# "This would create second object of Grad class"
+student2 = Grad("Swati", "Columbia University")
+student1.displayGrad()
+student2.displayGrad()
+print "Total students graduating: %d" % Grad.studentCount
+ 
