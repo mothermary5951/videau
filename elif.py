d6857af9fe7eda318a11af9bff76c285cd71babd
Lizbeth Videau <mothermary5951@gmail.com>
2015-04-06 16:53:56 -0400

    good elif and else examples

---
+++ elif.py
@@ -0,0 +1,15 @@
+var = raw_input("?")
+
+if var == ("100"):
+    print "var1 gets you a False expression value."
+    print var
+elif var == ("200"):
+    print "var2 gets you a False expression value."
+    print var
+elif var == ("5"):
+    print "Finally! a True expression value."
+    print var
+else:
+    print "Forget it. Another False expresion value."
+    print var
+print "All done."
