7289407aabace1ee4dcb5045d0c9b5d80ff51c4a
mothermary5951 <mothermary5951@users.noreply.github.com>
2015-04-08 16:12:25 -0400

    Update nested_forloops.py

--- nested_forloops.py
+++ nested_forloops.py
@@ -2,7 +2,7 @@ for x in xrange(1, 6):
      if x == 5:    ## multiplies by 1 through 4; stops at 5
          break
      for y in xrange(1, 6): ## stopped  at 6
-          if y == 6:     ## so only multiplies by 5!!!
+          if y == 6:     
               break
           else:
               print '%d * %d = %d' % (x, y, x * y)
