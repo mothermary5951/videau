f6b678322cc6b170ad8a8bb64104b2a539938b7c
Lizbeth Videau <mothermary5951@gmail.com>
2015-04-08 17:09:41 -0400

    More efficient code for executing2 or 3 functions on parent and child classes

--- class_practice.py
+++ class_practice.py
@@ -7,32 +7,44 @@ class protein:
         print "This protein has %d aminos" % self.amino_acids
 
     def count_helix(self):
-        return
-##      print "This protein has %d helices" % self.helix_number
+        print "This protein has %d helices" % self.helix_number
+
+    def printguts(self):
+        self.fold()
+        self.count_helix()
 
 class subunit(protein):
     def domain(self):
         print "%s is chain id" % self.chainid
 
 
+    def printguts(self):
+        self.fold()
+        self.count_helix()
+        self.domain()
+
+
 betasheet = subunit()
 betasheet.amino_acids = 28
 betasheet.helix_number = 0
 betasheet.chainid = ['D']
-betasheet.fold()
-betasheet.count_helix()
-betasheet.domain()
+## betasheet.fold()
+##betasheet.count_helix()
+##betasheet.domain()
+betasheet.printguts()
+
     
 ricin = protein()
 ricin.amino_acids = 267
 ricin.helix_number = 5
-ricin.fold()
-ricin.count_helix()
+##ricin.fold()
+##ricin.count_helix()
+ricin.printguts()
 
 hemoglobin = protein()
 hemoglobin.amino_acids = 102
 hemoglobin.helix_number = 3
-hemoglobin.fold()
-hemoglobin.count_helix()
+##hemoglobin.fold()
+##hemoglobin.count_helix()
+hemoglobin.printguts()
 
-print "'%s', '%s', and '%s' have %d, %d, and %d amino acids, respectively." % ['betasheet', 'ricin', 'hemoglobin'].fold() 
