f13d7c44bda0d24ac677749bfd53fe463784f490
Lizbeth Videau <mothermary5951@gmail.com>
2015-04-14 14:35:24 -0400

    Most recent attempt at list manipulation

--- stage2_anotherclass.py
+++ stage2_anotherclass.py
@@ -1,7 +1,5 @@
 class Appliance:
-    "Pricing, power source, and store location for all household appliances"
-  ##  electricCount = 0                                                                                              
-
+    "Pricing, power source, and store location for all household appliances"                                                                                              
     def __init__(self, name, price, energy, store):
         self.name = name
         self.price = price
@@ -12,47 +10,41 @@ class Appliance:
     def displayAppliance(self):
         print "Item: ", self.name, "Price: ", self.price, "Energy Source: ", self.energy, "Store: ", self.store
 
-items = []
-
-items.append(Appliance("stove", "$650", "electric or gas", "Wright Appliance"))
-items.append(Appliance("refrigerator", "$825", "electric", "Home Depot"))
-items.append(Appliance("washer", "$500", "electric", "Sears"))
-items.append(Appliance("dryer", "$425", "electric or gas", "Sears"))
-items.append(Appliance("water heater", "$490", "electric or gas", "Lowe's"))
-items.append(Appliance("air conditioner", "$1100", "electric", "Wright Appliance"))
-items.append(Appliance("oil heater", "$200", "electric", "Lowe's"))
-
-
 class Appliances:
     "List object to put list objects in"
     electricCount = 0
+    items = []
 
-    def __init__(self, Appliance):
-        
-        returns ['name']
+    def condense(name, price, energy, store):
+        purchase = Appliance(name, price, energy, store)
+
+        items.append(purchase)    
+
+        if energy == "electric":
+            electricCount += 1
+
+        elif energy == "electric or gas":
+            electricCount += 1
+
+        else:
+            electricCount += 0
 
     def displayCount(self):
         print "Total electric appliances available: %d" % Appliances.electricCount
 
-
     def displayAppliances(self):
         print "Item: ", self.name, "Price: ", self.price, "Energy Source: ", self.energy, "Store: ", self.store
- 
-## items = []
 
-## items.append(Appliance("stove", "$650", "electric or gas", "Wright Appliance"))
-## items.append(Appliance("refrigerator", "$825", "electric", "Home Depot"))
-## items.append(Appliance("washer", "$500", "electric", "Sears"))
-## items.append(Appliance("dryer", "$425", "electric or gas", "Sears"))
-## items.append(Appliance("water heater", "$490", "electric or gas", "Lowe's"))
-## items.append(Appliance("air conditioner", "$1100", "electric", "Wright Appliance"))
-## items.append(Appliance("oil heater", "$200", "electric", "Lowe's"))
+items.condense("stove", "$650", "gas", "Wright Appliance")
+        items.condense("refrigerator", "$825", "electric", "Home Depot")
+        items.condense("washer", "$500", "electric", "Sears")
+        items.condense("dryer", "$425", "electric or gas", "Sears")
+        items.condense("water heater", "$490", "gas", "Lowe's")
+        items.condense("air conditioner", "$1100", "electric", "Wright Appliance")
+        items.condense("oil heater", "$200", "electric", "Lowe's")
 
-print "Here are the %d appliances common in households:" % Appliances.electricCount
+       print  "Here are the %d appliances common in households:" % Appliances.electricCount
 
-for name in items:
+        for name in items:
 
-    name.displayAppliance()
-
-
-## appliances = "stove,refrigerator,washer,dryer,water heater,air conditioner,heater" 
+            name.displayAppliance()
