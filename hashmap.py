ca83d782b8fd85eaa4dd863f490f714462cf485f
Lizbeth Videau <mothermary5951@gmail.com>
2015-04-21 12:21:42 -0400

    Working early version of script for ex39_dicttest.py

--- hashmap.py
+++ hashmap.py
@@ -1,15 +1,15 @@
 def new(num_buckets=256):               # creates a new function with the potential for 256 (2 to the 7th) containers
     """Initializes a Map with the given number of buckets."""   #  python uses 'map' instead of 'dictionary'  
     aMap = []                           # variable 'aMap' starts as an empty list
-    for i in range(0, num_buckets):    # for integer in range of 0 to 256,
-        aMap.append([])                # append new buckets to variable list 'aMap' and
+    for i in range(0, num_buckets):    # for integer in range of 0 to 256, makes a bucketlist to
+        aMap.append([])                # append new buckets to variable list 'aMap' within the function 'new' and
     return aMap                    # returns the total list aMap increased by one.
 
-def hash_key(aMap, key):              #creates new function 'hash_key' requiring 2 arguments: a new key and the aMap list
-    """Given a key, this will create a number and then convert it to an index for the aMap's buckets."""
+def hash_key(aMap, key):              #'hash is a builtin function. 'hash_key' requires 2 arguments: a new key and the aMap list
+    """Given a key, this will create a number and then convert it to an index for the aMap's buckets.""" ## hash converts string to integer
     return hash(key) % len(aMap)   #  returns the modulo remainder to be sure the index fits within the aMap larger string
 
-def get_bucket(aMap, key):        # 'Get' is a builtin method for finding values:  this 'get' matches a key to its bucket 
+def get_bucket(aMap, key):        # 'Get' is a builtin method for finding values:  this 'get' matches a key to where its bucket could be
     """Given a key, find the bucket where it would go."""
     bucket_id = hash_key(aMap, key) # the bucket_id is defined by calling hash_key on a specific key in aMap list.
     return aMap[bucket_id]      #  Adds the bucket_id to the aMap list
@@ -24,7 +24,7 @@ def get_slot(aMap, key, default=None):  #  Slots are subdivisions within bucket,
     for i, kv in enumerate(bucket):   # iterates through the 3 features 'enumerated' in a general slot in a bucket,
         k, v = kv                     #  where key, value = kv
         if key == k:                 # if specified key is recognized, then
-            return i, k, v          # return all 3 features in the bucket's slot on the aMap list, 
+            return i, k, v          # return the 3-feature tuple that is in the bucket's slot on the aMap list, 
 
     return -1, key, default       # if it's not, the function returns the default "none"
 
@@ -33,14 +33,14 @@ def get(aMap, key, default=None):       #  fetches the list aMap and its keys or
     i, k, v = get_slot(aMap, key, default=default)  #  calls get_slot to identify the 3 features of each slot
     return v              # returns only the value in a given slot
 
-def set(aMap, key, value):      # 'set' is a builtin class
+def set(aMap, key, value):      # new function called set;  not the python builtin class
     """Sets the key to the value, replacing any existing value."""
     bucket = get_bucket(aMap, key)   # names get_bucket return 'bucket'
     i, k, v = get_slot(aMap, key)    # names get_slot return 'i, k, v'
 
     if i >= 0:                        # for slot-integer numbers >= 0, set the key and value in place of the integer
         # the key exists, replace it.
-        bucket[i] = (key, value)
+        bucket[i] = (key, value)    # sets one integer,one key, and one value per bucket, overwriting whatever is in there before.
     else:
         # the key does not, append to create it    # if the key does not exist, make a new bucket and add it to aMap list
         bucket.append((key, value))
@@ -55,12 +55,24 @@ def delete(aMap, key):          #  deletes a key from the aMap list
             del bucket[i]        #  then delete the bucket
             break               # and then stop deleting
 
-def list(aMap):                 # print out the keys and values for each bucket in the aMap list
+def dump(aMap, key, value):                 # print out the keys and values for each bucket in the aMap list                                   
     """Prints out what's in the Map."""
-    for bucket in aMap:
+    bucket = get_bucket(aMap, key)   # names get_bucket return 'bucket'   START HERE ....                                                           i, k, v = get_slot(aMap, key)    # names get_slot return 'i, k, v'              
+    for i, kv in enumerate(bucket):
+        k, v = kv
+        bucket = i, k, v
         if bucket:
-            for k, v in bucket:
-                print k, v
+            ## for i, k, v in bucket:
+            print i, k, v
+
+
+
+## def list(aMap):                 # print out the keys and values for each bucket in the aMap list
+   ##  """Prints out what's in the Map."""
+   ##  for bucket in aMap:
+   ##      if bucket:
+   ##          for k, v in bucket:
+   ##              print k, v
 
 
 ##  NOTE ON 'HASH':  Hash is a python builtin function that (among other things) assigns an integer to a string to establish its
